aa6e0b33db0ddfea4b0ef25b3cb880c9b654233c
aki <akshitpjt07@gmail.com>
2024-02-02 12:04:36 +0530

    component is made

--- client/movie/src/App.jsx
+++ client/movie/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import Data from '../Data.json'
 
 function App() {
 
   return (
     <>
   <div>INSPIRATIONAL-MOVIES</div>
+  <div>
+    <h1>{Data[0].MOVIE_NAME}</h1>
+    <h1>{Data[0].IMDb_RATINGS}</h1>
+    <h1>{Data[0].ROTTEN_TOMATOES_RATING}</h1>
+    <h1>{Data[0].RELEASE_DATE}</h1>
+    <h1>{Data[0].OFFICIAL_LANGUAGE}</h1>
+    <h1>{Data[0].AWARD_WON}</h1>
+  
+  
+  
+  
+  
+  </div>
 
     </>
   )
