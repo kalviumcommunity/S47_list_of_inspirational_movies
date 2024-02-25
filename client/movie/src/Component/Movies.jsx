2b04c34f9776d7bd647d439bef1196c36a9d607c
aki <akshitpjt07@gmail.com>
2024-02-25 23:18:27 +0530

    post running

--- client/movie/src/Component/Movies.jsx
+++ client/movie/src/Component/Movies.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import {BrowserRouter as Routes, Route, Link, Router } from 'react-router-dom'
+import Add from './Add'
 
-export default function BrowseStories() {
+export default function Movies() {
         const [users, setUsers] = useState([])
         useEffect(()=>{
             fetch("http://localhost:3000/getting")
@@ -10,24 +12,35 @@ export default function BrowseStories() {
         },[])
     return (
         <div>
+               
+
+            
             <div>
                 <table>
                     <tbody>
                         {users.map((data) => {
                             return (
-                                <tr key={data.ID}>
-                                    <td>{data.MOVIE_NAME}</td>
-                                    <td>{data.IMDb_RATINGS}</td>
-                                    <td>{data.ROTTEN_TOMATOES_RATING}</td>
-                                    <td>{data.ELEASE_DATE}</td>
-                                    <td>{data.OFFICIAL_LANGUAGE}</td>
-                                    <td>{data.AWARD_WON}</td>
+                                <>
+                
+                                    <tr key={data.ID}>
+                                    <td>{data.MOVIE_NAME} <hr /> <br /> </td>
+                                    
+                                    <td>{data.IMDb_RATINGS}<hr /> <br /></td>
+                                    <td>{data.ROTTEN_TOMATOES_RATING}<hr /> <br /></td>
+                                    <td>{data.ELEASE_DATE}<hr /> <br /></td>
+                                    <td>{data.OFFICIAL_LANGUAGE}<hr /> <br /></td>
+                                    <td>{data.AWARD_WON}<hr /> <br /></td>
+                                    <button>Update</button>
+                                    <button>Delete</button>
                                 </tr>
+
+                                </>
                             )
                         })}
                     </tbody>
                 </table>
             </div>
+
 
         </div>
     )
